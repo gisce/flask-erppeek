f81dd0920ab085305d290570962724d1618ff9fc
Eduard Carreras <ecarreras@gisce.net>
2017-08-28 15:26:54 +0200

    Bump to v1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask-erppeek',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/gisce/flask-erppeek',
     license='BSD',
     author='GISCE-TI, S.L.',
